530668413c3d5aa0d8fafaaf63dcc478c8b38146
Garrett Griffey <garrettwgriffey@gmail.com>
2020-02-22 13:12:27 -0600

    creation of repo

---
+++ main.js
@@ -0,0 +1,19 @@
+// var letters = 'abcdefghijklmnopqrstuvwxyz';
+// var numbers = '1234567890';
+// var special = '~!@#$%^&*()_-+=[]{};:/?>.<,'
+// var random = '';
+// var charSet = (letters + numbers + special).split('');
+// var wordLen = 20;
+// for(var i = 0; i < wordLen; i ++){
+//     var randIndex = Math.floor(Math.random() * charSet.length);
+//     random += charSet[randIndex];
+// }
+// console.log(random);
+
+var passLength = prompt("How many characters do you want?");
+var numOfNums = prompt("How many numbers do you want?");
+var upperCase = prompt("How many upper case letters do you want?");
+var lowerCase = prompt("How many lower case letters do you want?");
+var specChar = prompt("How many special characters do you want?")
+
+
