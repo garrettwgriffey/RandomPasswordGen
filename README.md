389f7f539a9bfd7c1bb5f91df3d8af0c2ed741db
Garrett Griffey <garrettwgriffey@gmail.com>
2020-02-25 18:09:05 -0600

    turn in

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # RandomPasswordGen
-Produces random password with info entered by user
+
+### Contributors 
+#### Garrett Griffey https://garrettwgriffey.github.io/
+
+### Technology Stack 
+#### HTML, CSS, Javascript
+
+### About
+#### Produces random password with info entered by user
+
+### Contact 
+#### Garrett Griffey (@garrettwgriffey)
+#### Garrettwgriffey@gmail.com
--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+// generate button
+var generateBtn = document.querySelector("#generate");
+var lowerCaseValue;
 var upperCaseValue;
-var numberCaseValue;
+var numOfNumsValue;
 var specialCharValue;
 var lengthValue;
 
-// generate button
-var generateBtn = document.querySelector("#generate");
+
 
 // displays password
 function writePassword () {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
+
 }
 
 
 
 // criteria prompts
+
+
 function generatePassword() {
 
 
@@ -29,14 +34,16 @@ function generatePassword() {
         var lowerCase = confirm("Do you want lowercase letters?");
         var numOfNums = confirm("Do you want numbers?");
         var specialChar = confirm("Do you want special characters?");
-    } else {
-      alert("Too few characters little buddy!")
+    } 
+    else {
+      alert("Too few characters, little buddy!")
       return generatePassword();
     }
 
     
     if (upperCase===true) {
       upperCaseValue = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
       for (i = 0; i < upperCaseValue.length; i++) {
         newPassword.push(upperCaseValue[i]);
       }
@@ -45,6 +52,7 @@ function generatePassword() {
     
     if (lowerCase===true) {
       lowerCaseValue = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
       for (i = 0; i < lowerCaseValue.length; i++) {
         newPassword.push(lowerCaseValue[i]);
       }
@@ -52,6 +60,7 @@ function generatePassword() {
 
     if (numOfNums === true) {
       numOfNumsValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
       for (i = 0; i < numOfNumsValue.length; i++) {
         newPassword.push(numOfNumsValue[i]);
       }
@@ -59,10 +68,12 @@ function generatePassword() {
     
     if (specialChar === true) {
       specialCharValue = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "'", "<", ">", "?", "/", "`", "~"];
+
       for (i = 0; i < specialCharValue.length; i++) {
         newPassword.push(specialCharValue[i]);
       }
     }
+
     if (upperCase !== true && lowerCase !== true && numOfNums !== true && specialChar !== true) {
       alert("You did not choose any character types! Please try again.");
       return generatePassword();
@@ -76,11 +87,12 @@ function generatePassword() {
   //   return generatePassword();
   // }
 
-  var randomizer =""
+  var randomizer ="";
+
   for (var i = 0; i < lengthValue; i++) {
     randomizer += newPassword[Math.floor(Math.random() * newPassword.length)];
-
-  return randomizer;
+    return randomizer;
+    
   }
 
 
